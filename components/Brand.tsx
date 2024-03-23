ad504880d8f0d97a8b787e2367c91e832e1e2c5b
Victor Lima <victorl1ma@hotmail.com>
2024-03-23 19:27:05 -0300

    sync

---
+++ components/Brand.tsx
@@ -0,0 +1,66 @@
+export default function Brand() {
+  return (
+    <div className="w-full flex flex-row p-6 pl-12 pr-12">
+      <div className="flex flex-row items-center gap-1 justify-center">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M18.2 12.27L20 6H4L5.8 12.27C5.85829 12.4779 5.98233 12.6614 6.15358 12.793C6.32482 12.9246 6.53406 12.9972 6.75 13H17.25C17.4677 12.9994 17.6792 12.9278 17.8524 12.796C18.0257 12.6643 18.1512 12.4796 18.21 12.27H18.2Z"
+            stroke="#581C87"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M8 13V22"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M16 22V13"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M9 6L10 13"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M15 6L14 13"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M12 6V2"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+          <path
+            d="M13 2H11"
+            stroke="#581C87"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+
+        <span className="text-purple-900 font-bold text-xl">Lytical</span>
+      </div>
+    </div>
+  );
+}
---
+++ components/Copyright.tsx
@@ -0,0 +1,12 @@
+export default function Copyright() {
+  return (
+    <div className="w-full flex flex-row p-6 pl-12 pr-12 bg-neutral-900">
+      <div className="flex flex-row items-center gap-1 w-full justify-center gap-48">
+        <span className="text-sm text-neutral-500">Lytical © 2024</span>
+        <a className="text-sm text-neutral-500" href="/cookies">
+          Preferências de Cookies
+        </a>
+      </div>
+    </div>
+  );
+}
